bc1f3b3e4fb48c6ebfb80a90c954482afb259930
duraanos <duran364@anadolu.edu.tr>
2025-07-28 20:59:46 +0300

    feat(component): Create HeroButtonGroup component

---
+++ components/HeroButtonGroup.tsx
@@ -0,0 +1,12 @@
+export default function HeroButtonGroup() {
+  return (
+    <div className="flex gap-[20px]">
+      <button className="bg-customLime-950 text-white rounded-[16px] font-medium px-[10px] py-[10px]">
+        Get Started
+      </button>
+      <button className="border-2 border-customLime-950 rounded-[16px] font-medium px-[10px] py-[10px]">
+        Watch Video
+      </button>
+    </div>
+  );
+}
