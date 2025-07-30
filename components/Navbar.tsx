82295f53a64ae70521dc09ab217dd020b8896298
duraanos <duran364@anadolu.edu.tr>
2025-07-30 10:49:18 +0300

    style(ui): Add items-center utility for vertical alignment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarButtonGroup from './NavbarButtonGroup';
 
 export default function Navbar() {
   return (
-    <nav className='flex justify-between'>
+    <nav className='flex justify-between items-center'>
       <Logo />
       <NavbarButtonGroup />
     </nav>
