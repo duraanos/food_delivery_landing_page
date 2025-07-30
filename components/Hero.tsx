8e391ccfebe215c53ee7e309ec0d1bada38de2bb
duraanos <duran364@anadolu.edu.tr>
2025-07-30 15:26:09 +0300

    feat(hero): Add MealCard component to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import HeroButtonGroup from './HeroButtonGroup';
 import CustomerIcons from './CustomerIcons';
+import MealCard from './MealCard';
 
 export default function Hero() {
   return (
@@ -9,8 +10,7 @@ export default function Hero() {
         <div className="flex flex-col w-1/2 gap-[20px]">
           <div className="flex flex-col gap-[5px]">
             <h1 className="text-customGray-900 font-bold text-4xl">
-              Skip the Line, <br></br>
-              Savor the Taste
+              Skip the Line, <br></br> Savor the Taste
             </h1>
             <p className="text-xl">
               No more waiting, no more stress <br></br> order ahead, track your
@@ -25,14 +25,20 @@ export default function Hero() {
             </p>
           </div>
         </div>
-        <div className="relative flex justify-center w-1/2">
+        <div className="relative flex justify-center">
+          <MealCard
+            image="/images/meal-1.png"
+            meal="Traditional Pasta"
+            price="$30.00"
+            className="absolute right-60 bottom-10 z-20"
+          />
           <div className="absolute right-10 bottom-0 bg-customLime-700 w-[475] h-[475] rounded-full blur-[100px]"></div>
           <Image
             src="/images/hero.png"
             alt="hero_image"
             width={400}
             height={400}
-            className='z-10'
+            className="z-10"
           />
         </div>
       </div>
