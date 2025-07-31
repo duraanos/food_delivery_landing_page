1e96ad5178c5f0e5c77e820ad895923ec5ff5b3d
duraanos <duran364@anadolu.edu.tr>
2025-07-31 19:12:15 +0300

    feat(page): Add LetsDoItTogetherSection component to layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Container from '@/components/layout/Container';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import HowToWorkSection from '@/components/HowToWorkSection';
+import LetsDoItTogetherSection from '@/components/LetsDoItTogetherSection';
 
 function Home() {
   return (
@@ -16,6 +17,11 @@ function Home() {
         <Container>
           <HowToWorkSection />
         </Container>
+        <div className="bg-[url('/images/wavy_bg.png')] bg-cover  w-full">
+          <Container>
+            <LetsDoItTogetherSection />
+          </Container>
+        </div>
       </main>
     </>
   );
