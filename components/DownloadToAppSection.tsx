cc9695ef4ed60b71603be3d801befbe6d8e6b24b
duraanos <duran364@anadolu.edu.tr>
2025-08-01 11:45:13 +0300

    feat(components): Create DownloadToApp component

---
+++ components/DownloadToAppSection.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function DownLoadToAppSection() {
+  return (
+    <section className="mb-32">
+      <div className="flex flex-col items-center justify-between gap-[5px]">
+        <div>
+          <Link href={'/'}>
+            <Image
+              src="/images/download_to_app.svg"
+              alt="download_to_app"
+              width={225}
+              height={225}
+            />
+          </Link>
+        </div>
+        <h3 className="font-bold text-3xl text-customLime-900">
+          Download to App
+        </h3>
+        <p className="text-customLime-900 text-xl">
+          Order food faster, track deliveries in real-time, and unlock exclusive
+          app-only deals.
+        </p>
+        <div className="flex gap-5">
+          <Link href={'/'}>
+            <Image
+              src={'/images/app_store.svg'}
+              alt="app_store"
+              width={200}
+              height={100}
+            />
+          </Link>
+          <Link href={'/'}>
+            <Image
+              src={'/images/google_play.svg'}
+              alt="google_play"
+              width={200}
+              height={100}
+            />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
