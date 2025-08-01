0a5190dc58ecfd1bb8f49c5f83a5737c3e0262fe
duraanos <duran364@anadolu.edu.tr>
2025-08-01 12:12:52 +0300

    feat(navigation): Define navigation links structure for footer

---
+++ lib/utils.ts
@@ -0,0 +1,19 @@
+export const navigation = {
+  feature: [
+    { name: 'Utility', href: '#' },
+    { name: 'Tokekonomics', href: '#' },
+    { name: 'Roadmap', href: '#' },
+    { name: 'Integrations', href: '#' },
+  ],
+  company: [
+    { name: 'About us', href: '#' },
+    { name: 'Features', href: '#' },
+    { name: 'Blog', href: '#' },
+  ],
+  help: [
+    { name: 'Privacy Policy', href: '#' },
+    { name: 'Terms & Conditions', href: '#' },
+    { name: 'Changelog', href: '#' },
+    { name: 'License', href: '#' },
+  ],
+};
