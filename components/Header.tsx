0d68f47a7485b380ede674aa75387bcc20407418
duraanos <duran364@anadolu.edu.tr>
2025-08-04 12:24:51 +0300

    refactor(header): Remove unused Container import and add className

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,8 @@
-import Container from './layout/Container';
 import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <Navbar />
     </header>
   );
