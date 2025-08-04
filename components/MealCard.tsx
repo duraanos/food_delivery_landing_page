946195c9eaa3bb4af76392b39e7c38340921622a
duraanos <duran364@anadolu.edu.tr>
2025-08-04 17:28:05 +0300

    refactor(meal-card): Update props to use a single props object with MealCardProps interface

--- components/MealCard.tsx
+++ components/MealCard.tsx
@@ -7,21 +7,16 @@ interface MealCardProps {
   className: string;
 }
 
-export default function MealCard({
-  image,
-  meal,
-  price,
-  className,
-}: MealCardProps) {
+"export default function MealCard(props: MealCardProps) {
   return (
-    <div className={`hero-mealCard ${className}`}>
+    <div className={`hero-mealCard ${props.className}`}>
       <div className="hero-mealCardImage">
-        <Image src={image} alt="meal-1" width={65} height={65} />
+        <Image src={props.image} alt="meal-1" width={65} height={65} />
       </div>
       <div className="hero-mealCardItem">
-        <p className="hero-mealCardMealName">{meal}</p>
+        <p className="hero-mealCardMealName">{props.meal}</p>
         <div className="hero-mealCardBox">
-          <p className="hero-mealCardPrice">{price}</p>
+          <p className="hero-mealCardPrice">{props.price}</p>
           <p className="hero-mealCardUnderlined ">Order Now</p>
         </div>
       </div>
