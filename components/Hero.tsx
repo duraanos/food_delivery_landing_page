4a167b17926d2f0d73ab6e71bca3d49bf7b4b18f
duraanos <duran364@anadolu.edu.tr>
2025-08-04 13:49:57 +0300

    refactor(hero): Update classNames for improved readability and styling consistency

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,46 +6,46 @@ import MealCard from './MealCard';
 export default function Hero() {
   return (
     <section>
-      <div className="flex items-center">
-        <div className="flex flex-col w-1/2 gap-[20px]">
-          <div className="flex flex-col gap-[5px]">
-            <h1 className="text-customGray-900 font-bold text-5xl">
+      <div className="hero">
+        <div className="hero-banner">
+          <div className="hero-bannerTexts">
+            <h1 className="hero-title">
               Skip the Line, <br></br> Savor the{' '}
               <strong className="underline decoration-customLime-700">
                 Taste
               </strong>
             </h1>
-            <p className="text-base">
+            <p className="hero-description">
               No more waiting, no more stress <br></br> order ahead, track your
               meal in real time...
             </p>
           </div>
           <HeroButtonGroup />
-          <div className="flex items-center gap-3">
+          <div className="hero-customers">
             <CustomerIcons />
-            <p className="text-customLime-700 text-base font-bold">
+            <p className="hero-customerDescription">
               500K+ Our Happy Customers
             </p>
           </div>
         </div>
-        <div className="relative flex justify-center">
+        <div className="hero-images">
           <MealCard
             image="/images/meal-1.png"
             meal="Traditional Pasta"
             price="$30.00"
-            className="absolute right-60 bottom-10 z-20"
+            className="hero-imagesCustomClass1"
           />
           <MealCard
             image="/images/meal-2.png"
             meal="Fish Dish"
             price="$30.00"
-            className="absolute left-[262px] bottom-[225px] z-10"
+            className="hero-imagesCustomClass2"
           />
           <MealCard
             image="/images/meal-3.png"
             meal="Steak Frites"
             price="$30.00"
-            className="absolute left-72 bottom-4 z-20"
+            className="hero-imagesCustomClass3"
           />
           <div className="absolute -right-10 bottom-10 bg-customLime-700 w-[475] h-[475] rounded-full blur-[70px]"></div>
           <Image
@@ -53,7 +53,7 @@ export default function Hero() {
             alt="hero_image"
             width={400}
             height={400}
-            className="z-10 w-full"
+            className="hero-imageCustom"
           />
         </div>
       </div>
