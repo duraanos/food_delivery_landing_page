91d3299ae721645c757f2bb55ef7d7c215c906c5
duraanos <duran364@anadolu.edu.tr>
2025-08-04 14:23:28 +0300

    refactor(howtowork-card): Simplfy and standardize class names for improved readability and maintainability

--- components/HowToWorkCard.tsx
+++ components/HowToWorkCard.tsx
@@ -12,19 +12,19 @@ export default function HowToWorkCard({
   description,
 }: HowToWorkCardProps) {
   return (
-    <div className="bg-gradient-to-t from-customGreen-100 from-19% to-customLime-100 to-66% flex flex-col items-center p-[20px] rounded-2xl">
+    <div className="howtowork-card">
       <div>
         <Image
           src={iconSrc}
           alt={title}
           width={100}
           height={100}
-          className="text-customLime-900"
+          className="howtowork-cardImageCustomClass"
         />
       </div>
-      <div className="flex flex-col items-center">
-        <h3 className="text-customLime-900 font-semibold text-xl mb-[16px]">{title}</h3>
-        <p className="text-customLime-900 text-base">{description}</p>
+      <div className="howtowork-cardItem">
+        <h3 className="howtowork-cardTitle">{title}</h3>
+        <p className="howtowork-cardDescription">{description}</p>
       </div>
     </div>
   );
