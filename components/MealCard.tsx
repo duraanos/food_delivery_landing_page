037d37bb173f0f040e27bac762128bb3c66abb61
duraanos <duran364@anadolu.edu.tr>
2025-08-04 13:55:53 +0300

    refactor(meal-card): Simplfy and standardize MealCard class names

--- components/MealCard.tsx
+++ components/MealCard.tsx
@@ -14,17 +14,15 @@ export default function MealCard({
   className,
 }: MealCardProps) {
   return (
-    <div
-      className={`bg-white p-[10px] w-[300] h-[85] flex items-center gap-[10px] rounded-2xl ${className}`}
-    >
-      <div className="image">
+    <div className={`hero-mealCard ${className}`}>
+      <div className="hero-mealCardImage">
         <Image src={image} alt="meal-1" width={65} height={65} />
       </div>
-      <div className="flex flex-col gap-[5px]">
-        <p className="font-medium">{meal}</p>
-        <div className="flex gap-[50px] items-center justify-center">
-          <p className="font-medium">{price}</p>
-          <p className="font-medium underline">Order Now</p>
+      <div className="hero-mealCardItem">
+        <p className="hero-mealCardMealName">{meal}</p>
+        <div className="hero-mealCardBox">
+          <p className="hero-mealCardPrice">{price}</p>
+          <p className="hero-mealCardUnderlined ">Order Now</p>
         </div>
       </div>
     </div>
