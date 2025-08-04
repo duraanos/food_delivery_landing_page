d086c5f32c5951de28ff110b5704026d40875a2d
duraanos <duran364@anadolu.edu.tr>
2025-08-04 17:30:22 +0300

    refactor(lets-do-it-together-card): Update props to use a single props object with LetsDoItTogetherCardProps interface

--- components/LetsDoItTogetherCard.tsx
+++ components/LetsDoItTogetherCard.tsx
@@ -6,19 +6,15 @@ interface LetsDoItTogetherCardProps {
   description: string;
 }
 
-export default function LetsDoItTogetherCard({
-  iconSrc,
-  title,
-  description,
-}: LetsDoItTogetherCardProps) {
+export default function LetsDoItTogetherCard(props: LetsDoItTogetherCardProps) {
   return (
-    <div className='letsdoittogether-card'>
+    <div className="letsdoittogether-card">
       <div>
-        <Image src={iconSrc} alt="rider" width={250} height={250} />
+        <Image src={props.iconSrc} alt="rider" width={250} height={250} />
       </div>
       <div className="letsdoittogether-cardItem">
-        <h3 className="letsdoittogether-cardTitle">{title}</h3>
-        <p className="letsdoittogether-cardDescription">{description}</p>
+        <h3 className="letsdoittogether-cardTitle">{props.title}</h3>
+        <p className="letsdoittogether-cardDescription">{props.description}</p>
       </div>
     </div>
   );
